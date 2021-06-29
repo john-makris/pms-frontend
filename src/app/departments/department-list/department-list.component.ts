87dcc8867018270f597a9e3c0af6d10ce490c3c9
john-makris <johnmakris.it@gmail.com>
2021-06-29 17:40:49 +0300

    Complete department-list component of department module

--- src/app/departments/department-list/department-list.component.ts
+++ src/app/departments/department-list/department-list.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
+import { SnackbarData } from 'src/app/common/snackbars/snackbar-data.interface';
+import { SnackbarService } from 'src/app/common/snackbars/snackbar.service';
+import { Department } from '../department.model';
+import { DepartmentService } from '../department.service';
 
 @Component({
   selector: 'app-department-list',
@@ -6,10 +15,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./department-list.component.css']
 })
 export class DepartmentListComponent implements OnInit {
+  displayedColumns = [
+    'id',
+    'name'
+  ];
+  dataSource = new MatTableDataSource<Department>();
+  private departmentsSnackbarSubscription!: Subscription;
 
-  constructor() { }
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(
+    private departmentService: DepartmentService,
+    private snackbarService: SnackbarService) { }
 
   ngOnInit(): void {
+    if(!this.dataSource.data.length) {
+      console.log("Departments list is empty");
+      this.departmentService.getAllDepartments()
+      .pipe(first())
+      .subscribe(departments => {
+        this.dataSource.data = departments;
+        console.log(this.dataSource.data);
+      });
+    }
+    this.departmentsSnackbarSubscription = this.snackbarService.snackbarState.subscribe(
+      (state: SnackbarData) => {
+        if(state.message.search('added' || 'updated' || 'deleted')) {
+          console.log("Snackbar: " + state.message);
+          this.departmentService.getAllDepartments()
+          .pipe(first())
+          .subscribe(departments => {
+            this.dataSource.data = departments;
+          });
+        }
+      }
+    );
   }
 
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  doFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    console.log("Department filter value: "+this.dataSource.filter);
+    console.log("Department value after filtering"+this.dataSource.data);
+  }
+
+  ngOnDestroy() {
+    if (this.departmentsSnackbarSubscription) {
+      this.departmentsSnackbarSubscription.unsubscribe();
+    }
+  }
 }
